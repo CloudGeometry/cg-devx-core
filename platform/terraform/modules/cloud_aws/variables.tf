c6988777359eb908b559110f2278457ca29014fe
vtsarfin-cg <vtsarfin@cloudgeometry.com>
2023-07-20 18:48:14 +0300

    some vars defined

--- platform/terraform/modules/cloud_aws/variables.tf
+++ platform/terraform/modules/cloud_aws/variables.tf
@@ -10,4 +10,33 @@ variable "hosted_zone_name" {
   type = string
 }
 
-#etc
+variable "cluster_network_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+  validation {
+    condition     = can(cidrnetmask(var.cluster_network_cidr))
+    error_message = "Must be a valid IPv4 CIDR block address."
+  }
+}
+
+variable "az_count" {
+  type    = number
+  default = 3
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "gxc"
+}
+variable "cluster_version" {
+  type = string
+}
+
+variable "node_groups" {
+  type = list(object({
+    }
+    )
+  )
+}
+
+
