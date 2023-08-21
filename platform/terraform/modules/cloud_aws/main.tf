f9ad5549c0ecbb7e2f7cf3f4e73959e8a4613a83
vtsarfin-cg <vtsarfin@cloudgeometry.com>
2023-08-21 12:00:34 +0300

    =>> module structure

--- platform/terraform/modules/cloud_aws/main.tf
+++ platform/terraform/modules/cloud_aws/main.tf
@@ -1,20 +1,3 @@
-provider "aws" {
-  region = local.region
-  default_tags { tags = local.tags } # all resources that implement tags, with the exception of the aws_autoscaling_group
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
-
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 locals {
