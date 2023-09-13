0a48734d9553f46e7644fc05b7b78a603c0f0bb6
Serg Shalavin <sshalavin@cloudgeometry.io>
2023-09-13 20:10:46 +0200

    add gitops_repo_html_url and gitops_repo_git_clone_url to tf output

--- platform/terraform/modules/vcs_github/repository/main.tf
+++ platform/terraform/modules/vcs_github/repository/main.tf
@@ -23,3 +23,11 @@ resource "github_repository" "repo" {
 output "repo_name" {
   value = github_repository.repo.name
 }
+
+output "repo_git_clone_url" {
+  value = github_repository.repo.git_clone_url
+}
+
+output "repo_git_html_url" {
+  value = github_repository.repo.html_url
+}
---
+++ platform/terraform/vcs/output.tf
@@ -0,0 +1,7 @@
+output "gitops_repo_git_clone_url" {
+  value = module.vcs.gitops_repo_git_clone_url
+}
+
+output "gitops_repo_html_url" {
+  value = module.vcs.gitops_repo_html_url
+}
