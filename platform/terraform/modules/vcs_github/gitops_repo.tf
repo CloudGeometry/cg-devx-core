1710ef21127cc7d4e229fb4e52b0f9e6058afaa3
Serg Shalavin <sshalavin@cloudgeometry.io>
2023-09-18 14:34:19 +0200

    add ssh_clone_url output

--- platform/terraform/modules/vcs_github/gitops_repo.tf
+++ platform/terraform/modules/vcs_github/gitops_repo.tf
@@ -16,3 +16,7 @@ output "gitops_repo_git_clone_url" {
 output "gitops_repo_html_url" {
   value = module.gitops-repo.repo_git_html_url
 }
+
+output "gitops_repo_ssh_clone_url" {
+  value = module.gitops-repo.repo_git_ssh_clone_url
+}
