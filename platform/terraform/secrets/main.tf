828602f088d6e6f792aacafb8b15890e9b4d6fb9
Serg Shalavin <sergs-pci@users.noreply.github.com>
2023-10-04 15:11:27 +0200

    vault module (#7)
    vault module
    
    ---------
    
    Co-authored-by: Serg Shalavin <sshalavin@cloudgeometry.io>

---
+++ platform/terraform/secrets/main.tf
@@ -0,0 +1,51 @@
+terraform {
+ 
+  # backend "s3" {
+  #   bucket = "<STATE_STORE_BUCKET>"
+  #   key    = "terraform/vault/terraform.tfstate"
+
+  #   region  = "<CLOUD_REGION>"
+  #   encrypt = true
+  # }
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+  }
+}
+
+# Configure Vault & Cloud Providers
+provider "vault" {
+  skip_tls_verify = "true"
+}
+
+provider "aws" {
+  region = "<CLOUD_REGION>"
+}
+
+locals {
+  cluster_name = "<PRIMARY_CLUSTER_NAME>"
+  workload_enabled = false
+  workloads   = local.workload_enabled == false ? {} : {
+    "workload-demo" = {
+    },
+  }
+}
+
+module "secrets" {
+  source = "../modules/secrets_vault"
+
+  cluster_name                 = local.cluster_name
+  workloads                    = local.workloads
+  cgdevxbot_ssh_public_key     = var.vcs_bot_ssh_public_key
+  cgdevxbot_ssh_private_key    = var.vcs_bot_ssh_private_key
+  b64_docker_auth              = var.b64_docker_auth
+  <GIT_PROVIDER>_token         = var.<GIT_PROVIDER>_token
+  atlantis_repo_webhook_secret = var.atlantis_repo_webhook_secret
+  atlantis_repo_webhook_url    = var.atlantis_repo_webhook_url
+  vault_token                  = var.vault_token
+}
---
+++ platform/terraform/secrets/variable.tf
@@ -0,0 +1,46 @@
+variable "vcs_bot_ssh_public_key" {
+  description = "public key for git operations"
+  type    = string
+  default = ""
+}
+
+variable "vcs_bot_ssh_private_key" {
+  description = "private key for git operations"
+  default = ""
+  type    = string
+  sensitive   = true
+}
+
+variable "<GIT_PROVIDER>_token" {
+  description = "token for git operations"
+  default = ""
+  type = string
+  sensitive   = true
+}
+
+variable "b64_docker_auth" {
+  description = "container registry auth"
+  default = ""
+  type = string
+  sensitive   = true
+}
+
+variable "atlantis_repo_webhook_secret" {
+  description = "atlantis webhook secret"
+  default = ""
+  type    = string
+  sensitive   = true
+}
+
+variable "atlantis_repo_webhook_url" {
+  description = "atlantis webhook url"
+  default = ""
+  type    = string
+}
+
+variable "vault_token" {
+  description = "vault token"
+  default = ""
+  type    = string
+  sensitive   = true
+}
