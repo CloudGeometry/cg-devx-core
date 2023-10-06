ba86d48797e2f3c59b24a4276d3a8dc0347d55e2
Turetskii Mikhail <mturetsky@cloudgeometry.io>
2023-10-06 16:26:51 +0300

    Azure sdk blueprint (#8)
    * Azure SDK pre-flight checks
    
    * GitLab manager implementation
    
    * Moved to poetry with project structure change
    
    ---------
    
    Co-authored-by: Alexander Ulyanov <alexu@cloudgeometry.io>

--- cli/common/const/namespaces.py
+++
@@ -1,5 +0,0 @@
-ARGO_WORKFLOW_NAMESPACE = "argo"
-ARGOCD_NAMESPACE = "argocd"
-ATLANTIS_NAMESPACE = "atlantis"
-EXTERNAL_SECRETS_OPERATOR_NAMESPACE = "external-secrets-operator"
-VAULT_NAMESPACE = "vault"
--- cli/common/utils/os_utils.py
+++
@@ -1,6 +0,0 @@
-from shutil import which
-
-
-def detect_command_presence(command: str) -> bool:
-    """Check whether `command` is on PATH and marked as executable."""
-    return which(command) is not None
