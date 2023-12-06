c4047d4af38bf15d26f4001b92f41a650bc95262
Serg Shalavin <sshalavin@cloudgeometry.io>
2023-12-06 21:25:04 +0100

    fix: sonarqube permission dependency

--- platform/terraform/modules/code_quality_sonarqube/project/main.tf
+++ platform/terraform/modules/code_quality_sonarqube/project/main.tf
@@ -26,10 +26,12 @@ resource "sonarqube_permissions" "workload-admins" {
   group_name  = "${var.project_name}-admins"
   project_key = sonarqube_project.this.project
   permissions = ["admin"]
+  depends_on  = [sonarqube_group.workload_admins]
 }
 
 resource "sonarqube_permissions" "workload-developers" {
   group_name  = "${var.project_name}-developers"
   project_key = sonarqube_project.this.project
   permissions = ["codeviewer", "issueadmin", "securityhotspotadmin", "scan"]
+  depends_on  = [sonarqube_group.workload_developers]
 }
