62bb2734f5539741ee8cd84250832ad21b119df3
Alex Ulyanov <alexu@cloudgeometry.io>
2023-12-11 23:05:24 +0100

    fix: update cluster node type and capacity to meet reservation

--- platform/terraform/modules/cloud_azure/variables.tf
+++ platform/terraform/modules/cloud_azure/variables.tf
@@ -41,20 +41,20 @@ variable "cluster_version" {
 variable "node_groups" {
   type = list(object({
     name           = optional(string, "default")
-    instance_types = optional(list(string), ["Standard_B2s"])
+    instance_types = optional(list(string), ["Standard_B2ms"])
     capacity_type  = optional(string, "Regular")
-    min_size       = optional(number, 5)
-    max_size       = optional(number, 9)
-    desired_size   = optional(number, 7)
+    min_size       = optional(number, 3)
+    max_size       = optional(number, 5)
+    desired_size   = optional(number, 3)
   }))
   default = [
     {
       name           = "default"
-      instance_types = ["Standard_B2s"]
+      instance_types = ["Standard_B2ms"]
       capacity_type  = "on_demand"
-      min_size       = 5
-      max_size       = 9
-      desired_size   = 7
+      min_size       = 3
+      max_size       = 5
+      desired_size   = 3
     }
   ]
 }
