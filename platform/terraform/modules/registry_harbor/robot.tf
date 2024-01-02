8ba9c9a3a168a8dd3b67c5ea7cddcefce364f3a9
Serg Shalavin <sshalavin@cloudgeometry.io>
2024-01-02 18:55:11 +0100

    fix: harbor robot permission and db restarts

--- platform/terraform/modules/registry_harbor/robot.tf
+++ platform/terraform/modules/registry_harbor/robot.tf
@@ -4,24 +4,77 @@ resource "harbor_robot_account" "main" {
   level       = "system"
   secret      = var.registry_main_robot_password
   #modify permissions to required later
+
   permissions {
-    access {
-      action   = "create"
-      resource = "labels"
-    }
-    kind      = "system"
-    namespace = "/"
-  }
-  permissions {
-    access {
-      action   = "pull"
-      resource = "repository"
-    }
-    access {
-      action   = "push"
-      resource = "repository"
-    }
     kind      = "project"
     namespace = "*"
-  }
+
+      access {
+        action   = "create"
+        resource = "scan"
+      }
+      access {
+        action   = "create"
+        effect   = "allow"
+        resource = "artifact-label"
+      }
+      access {
+        action   = "create"
+        effect   = "allow"
+        resource = "tag"
+      }
+      access {
+        action   = "delete"
+        effect   = "allow"
+        resource = "tag"
+      }
+      access {
+        action   = "list"
+        resource = "tag"
+      }
+      access {
+        action   = "list"
+        effect   = "allow"
+        resource = "artifact"
+      }
+      access {
+        action   = "list"
+        effect   = "allow"
+        resource = "repository"
+      }
+      access {
+        action   = "pull"
+        effect   = "allow"
+        resource = "repository"
+      }
+      access {
+        action   = "push"
+        effect   = "allow"
+        resource = "repository"
+      }
+      access {
+        action   = "read"
+        effect   = "allow"
+        resource = "artifact"
+      }
+      access {
+        action   = "stop"
+        resource = "scan"
+      }
+      access {
+        action   = "delete"
+        effect   = "allow"
+        resource = "artifact-label"
+      }
+      access {
+        action   = "delete"
+        effect   = "allow"
+        resource = "repository"
+      }
+      access {
+        action   = "delete"
+        effect   = "allow"
+        resource = "artifact"
+      }
+    }
 }
