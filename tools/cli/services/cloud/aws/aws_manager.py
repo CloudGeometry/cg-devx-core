25bffd24a184736f37eb0340b105f76ce85ca367
Serg Shalavin <sshalavin@cloudgeometry.io>
2024-02-02 19:24:10 +0100

    fix

--- tools/cli/services/cloud/aws/aws_manager.py
+++ tools/cli/services/cloud/aws/aws_manager.py
@@ -171,5 +171,10 @@ class AWSManager(CloudProviderManager):
         return ""
 
     @trace()
+    def create_iac_pr_automation_config_snippet(self):
+        return '''# aws specific section
+      # ----'''
+
+    @trace()
     def create_autoscaler_snippet(self, cluster_name: str, node_groups=[]):
         return '''awsRegion: <CLOUD_REGION>'''
