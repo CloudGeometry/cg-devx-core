ac4d7370c825e1381236c0c4d4457940f1736918
Alexander Ulyanov <alexu@cloudgeometry.io>
2024-02-02 18:12:29 +0100

    Feature/remove local workload branch
    Cleanup workload init / remove local feature branches to allow re-use of workload names

--- tools/cli/services/platform_gitops.py
+++ tools/cli/services/platform_gitops.py
@@ -7,6 +7,7 @@ from git import Repo, Actor
 from common.const.common_path import LOCAL_GITOPS_FOLDER, LOCAL_TF_FOLDER_VCS, LOCAL_TF_FOLDER_SECRETS_MANAGER, \
     LOCAL_TF_FOLDER_CORE_SERVICES, LOCAL_CC_CLUSTER_WORKLOAD_FOLDER
 from common.const.const import FALLBACK_AUTHOR_NAME, FALLBACK_AUTHOR_EMAIL
+from common.logging_config import logger
 from common.tracing_decorator import trace
 from services.vcs.git_provider_manager import GitProviderManager
 
@@ -23,6 +24,9 @@ class PlatformGitOpsRepo:
 
     @trace()
     def update(self):
+        """
+        Update local copy of platform GitOps repository
+        """
         with self._repo.git.custom_environment(GIT_SSH_COMMAND=self._ssh_cmd):
             # clean stale branches
             self._repo.remotes.origin.fetch(prune=True)
@@ -30,16 +34,30 @@ class PlatformGitOpsRepo:
             self._repo.remotes.origin.pull(self._repo.active_branch)
 
     @trace()
-    def branch_exist(self, branch_name):
+    def branch_exist(self, branch_name: str) -> bool:
+        """
+        Check if a given branch exists in the platform GitOps repository
+
+        :param branch_name: Branch name
+        :return: True, when branch exists, False otherwise
+        """
         return branch_name in self._repo.branches
 
     @trace()
-    def create_branch(self, branch_name):
+    def create_branch(self, branch_name: str):
+        """
+        Create a new branch in the platform GitOps repository
+
+        :param branch_name: Branch name
+        """
         current = self._repo.create_head(branch_name)
         current.checkout()
 
     @trace()
     def upload_changes(self):
+        """
+        Commit all the changes to an active branch
+        """
         with self._repo.git.custom_environment(GIT_SSH_COMMAND=self._ssh_cmd):
             self._repo.git.add(all=True)
             author = Actor(name=self._author_name, email=self._author_email)
@@ -49,14 +67,52 @@ class PlatformGitOpsRepo:
 
     @trace()
     def switch_to_branch(self, branch_name: str = "main"):
+        """
+        Switch to an existing branch in the platform GitOps repository
+
+        :param branch_name: Branch name
+        """
         self._repo.heads[branch_name].checkout()
 
     @trace()
+    def delete_branch(self, branch_name: str):
+        """
+        Delete an existing branch in the platform GitOps repository
+
+        :param branch_name: Branch name
+        """
+        try:
+            ref = self._repo.heads[branch_name]
+        except KeyError:
+            # suppress error as branch could've been deleted manually
+            logger.warning(f"Branch '{branch_name}' does not exist.")
+            return
+        # use force delete as branch is not fully merged yet
+        self._repo.delete_head(ref, force=True)
+        logger.debug(f"Branch '{branch_name}' successfully deleted.")
+
+    @trace()
     def create_pr(self, repo_name: str, head_branch: str, base_branch: str, title: str, body: str) -> str:
+        """
+        Create a PR/MR in the platform GitOps repository
+
+        :param repo_name: Repository name
+        :param head_branch: Head branch name
+        :param base_branch: Base branch name
+        :param title: PR/MR title
+        :param body: PR/MR body
+        """
         return self._git_man.create_pr(repo_name, head_branch, base_branch, title, body)
 
     @trace()
     def add_workload(self, wl_name: str, wl_repo_name: str, wl_gitops_repo_name: str):
+        """
+        Create variable files and core services configuration for a workload
+
+        :param wl_name: Workload name
+        :param wl_repo_name: Workload source code repository name
+        :param wl_gitops_repo_name: Workload GitOps repository name
+        """
         # repos
         self._add_wl_vars(LOCAL_TF_FOLDER_VCS, wl_name, {
             "description": f"CG DevX {wl_name} workload definition",
@@ -95,6 +151,11 @@ class PlatformGitOpsRepo:
 
     @trace()
     def rm_workload(self, wl_name: str):
+        """
+        Delete variable files and core services configuration of a workload
+
+        :param wl_name: Workload name
+        """
         # repos
         self._rm_wl_vars(LOCAL_TF_FOLDER_VCS, wl_name)
         # secrets
