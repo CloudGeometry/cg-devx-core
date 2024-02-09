3bcff473481e2e58c0e36a592331038df881829e
Serg Shalavin <sshalavin@cloudgeometry.io>
2024-02-09 12:50:20 +0100

    robot name fix

--- platform/terraform/modules/registry_harbor/project/vault.tf
+++ platform/terraform/modules/registry_harbor/project/vault.tf
@@ -1,6 +1,6 @@
 locals {
   b64_docker_auth = base64encode("robot@${var.project_name}+robot:${random_password.robot_password.result}")
-  push_b64_docker_auth = base64encode("push-robot@${var.project_name}+robot:${random_password.push_robot_password.result}")
+  push_b64_docker_auth = base64encode("robot@${var.project_name}+push-robot:${random_password.push_robot_password.result}")
 }
 
 resource "vault_generic_secret" "docker_config" {
@@ -33,7 +33,7 @@ resource "vault_generic_secret" "harbor_workload_robot_secret" {
       WL_ROBOT_NAME     = "robot@${var.project_name}+robot",
       WL_ROBOT_PASSWORD = random_password.robot_password.result,
       WL_ROBOT_B64_AUTH = local.b64_docker_auth,
-      WL_PUSH_ROBOT_NAME     = "push-robot@${var.project_name}+robot",
+      WL_PUSH_ROBOT_NAME     = "robot@${var.project_name}+push-robot",
       WL_PUSH_ROBOT_PASSWORD = random_password.push_robot_password.result,
       WL_PUSH_ROBOT_B64_AUTH = local.push_b64_docker_auth,
     }
